56b8d5a1449258abc2e0446b2cc0fcc82d29b827
Austin T <austin.james.tripp[at]gmail.com>
2024-11-10 17:05:47 +0000

    Initial conversion to use Jax instead of numpy.

--- kern_gp.py
+++ kern_gp.py
@@ -3,8 +3,8 @@ Contains code for zero-mean GP with kernal a*k(x,x) + s*I for some base kernel k
 """
 import logging
 
-import numpy as np
-from scipy.linalg import cho_solve, cholesky, solve_triangular
+import jax.numpy as jnp
+from jax.scipy.linalg import cho_solve, cholesky, solve_triangular
 
 LOWER = True
 logger = logging.getLogger(__name__)
@@ -15,7 +15,7 @@ def mll_train(a, s, k_train_train, y_train):
     L = _k_cholesky(k_train_train, s / a)
     data_fit = _data_fit(L, a, y_train)
     complexity = _complexity(L, a)
-    constant = -k_train_train.shape[0] / 2 * np.log(2 * np.pi)
+    constant = -k_train_train.shape[0] / 2 * jnp.log(2 * jnp.pi)
     return data_fit + complexity + constant
 
 
@@ -27,12 +27,12 @@ def noiseless_predict(a, s, k_train_train, k_test_train, k_test_test, y_train, f
     """
 
     L = _k_cholesky(k_train_train, s / a)
-    mean = np.dot(k_test_train, cho_solve((L, LOWER), y_train))
+    mean = jnp.dot(k_test_train, cho_solve((L, LOWER), y_train))
     covar_adj_sqrt = solve_triangular(L, k_test_train.T, lower=LOWER)
     if full_covar:
         covar_adj = covar_adj_sqrt.T @ covar_adj_sqrt
     else:
-        covar_adj = np.sum(covar_adj_sqrt**2, axis=0)
+        covar_adj = jnp.sum(covar_adj_sqrt**2, axis=0)
 
     return mean, a * (k_test_test - covar_adj)
 
@@ -40,18 +40,18 @@ def noiseless_predict(a, s, k_train_train, k_test_train, k_test_test, y_train, f
 def _k_cholesky(k, s):
     """Computes cholesky of k+sI."""
     logger.debug(f"Computing cholesky of {k.shape[0]}x{k.shape[0]} matrix with s={s}")
-    k2 = k + s * np.eye(k.shape[0])
+    k2 = k + s * jnp.eye(k.shape[0])
     L = cholesky(k2, lower=LOWER)
     logger.debug("Done computing cholesky")
     return L
 
 
 def _data_fit(L, a, y_train):
-    return -0.5 / a * np.dot(y_train.T, cho_solve((L, LOWER), y_train))
+    return -0.5 / a * jnp.dot(y_train.T, cho_solve((L, LOWER), y_train))
 
 
 def _complexity(L, a):
     """MLL complexity term for kernel a(L@L^T)"""
-    log_det_L = -np.sum(np.log(np.diag(L)))  # because we use cholesky, the factor of 2 cancels so no -1/2
-    a_adjustment = -np.log(a) * L.shape[0] / 2
+    log_det_L = -jnp.sum(jnp.log(jnp.diag(L)))  # because we use cholesky, the factor of 2 cancels so no -1/2
+    a_adjustment = -jnp.log(a) * L.shape[0] / 2
     return log_det_L + a_adjustment
--- test_kern_gp.py
+++ test_kern_gp.py
@@ -1,15 +1,16 @@
 import math
 
 import gpytorch
+import jax.numpy as jnp
 import numpy as np
 import pytest
 import torch
 
 from kern_gp import mll_train, noiseless_predict
 
-X_train = np.array([[1, 2, 3], [4, 5, 6]]) / 5
-y_train = np.array([0.5, 1.0])
-X_test = np.array([[3, 2, 1], [6, 5, 4]]) / 5
+X_train = jnp.array([[1, 2, 3], [4, 5, 6]]) / 5
+y_train = jnp.array([0.5, 1.0])
+X_test = jnp.array([[3, 2, 1], [6, 5, 4]]) / 5
 
 OUTPUTSCALE_LIST = [0.1, 1.0, 10.0]
 NOISE_LIST = list(OUTPUTSCALE_LIST)
@@ -18,8 +19,8 @@ NOISE_LIST = list(OUTPUTSCALE_LIST)
 class SimpleGP(gpytorch.models.ExactGP):
     def __init__(self, X_train, y_train, likelihood):
         super().__init__(
-            torch.as_tensor(X_train),
-            torch.as_tensor(y_train),
+            torch.as_tensor(np.array(X_train)),
+            torch.as_tensor(np.array(y_train)),
             likelihood,
         )
         self.mean_module = gpytorch.means.ZeroMean()
@@ -46,11 +47,12 @@ def test_mll_train(outputscale, noise):
     gp = _setup_gp(outputscale, noise)
     mll_obj = gpytorch.mlls.ExactMarginalLogLikelihood(gp.likelihood, gp)
     with gpytorch.settings.fast_computations(False, False, False), torch.no_grad():
-        mll_gpytorch = mll_obj(gp(torch.as_tensor(X_train)), torch.as_tensor(y_train)).item() * len(X_train)
+        mll_gpytorch = mll_obj(gp(torch.as_tensor(np.array(X_train))), torch.as_tensor(np.array(y_train))).item()
+        mll_gpytorch *= len(X_train)  # it returns the average, we want the sum
 
     # Make our GP and get its marginal likelihood
     with torch.no_grad():
-        k_train_train = gp.covar_module.base_kernel(torch.as_tensor(X_train)).to_dense().numpy()
+        k_train_train = gp.covar_module.base_kernel(torch.as_tensor(np.array(X_train))).to_dense().numpy()
     our_mll = mll_train(outputscale, noise, k_train_train, y_train)
 
     # Test: are they close?
@@ -65,22 +67,22 @@ def test_noiseless_predict(outputscale, noise, full_covar):
     gp = _setup_gp(outputscale, noise)
     gp.eval()
     with gpytorch.settings.fast_computations(False, False, False), torch.no_grad():
-        output = gp(torch.as_tensor(X_test))
+        output = gp(torch.as_tensor(np.array(X_test)))
         mu_true = output.mean.numpy()
         covar_true = output.covariance_matrix.numpy()
 
     # Get kernel matrices
     with torch.no_grad():
-        X_train_t = torch.as_tensor(X_train)
-        X_test_t = torch.as_tensor(X_test)
+        X_train_t = torch.as_tensor(np.array(X_train))
+        X_test_t = torch.as_tensor(np.array(X_test))
         k_train_train = gp.covar_module.base_kernel(X_train_t).to_dense().numpy()
         k_test_train = gp.covar_module.base_kernel(X_test_t, X_train_t).to_dense().numpy()
         k_test_test = gp.covar_module.base_kernel(X_test_t).to_dense().numpy()
 
     # Adjust matrices based on full covar
     if not full_covar:
-        k_test_test = np.diag(k_test_test)
-        covar_true = np.diag(covar_true)
+        k_test_test = jnp.diag(k_test_test)
+        covar_true = jnp.diag(covar_true)
 
     # Get predictions from our GP
     our_mu, our_covar = noiseless_predict(
@@ -88,5 +90,5 @@ def test_noiseless_predict(outputscale, noise, full_covar):
     )
 
     # Test: are they close?
-    assert np.allclose(mu_true, our_mu)
-    assert np.allclose(covar_true, our_covar, rtol=1e-5)
+    assert jnp.allclose(mu_true, our_mu)
+    assert jnp.allclose(covar_true, our_covar, rtol=1e-5)
